9402bab0e24e9f9fcc2583ed25dd23302fd2ef0d
Kunal Singh <158560481+kunalsinghrajput11@users.noreply.github.com>
2024-03-09 12:29:48 +0530

    Tic Tac Toe game Using HTML/ CSS/JS

---
+++ tic tac toe game project/app.js
@@ -0,0 +1,72 @@
+let boxes=document.querySelectorAll(".box");
+let resetbtn = document.querySelector("#reset-btn");
+let newgamebtn = document.querySelector("#new-btn");
+let masscontainer = document.querySelector(".mass-container");
+let massage=document.querySelector("#mass");
+
+
+let TurnO=true;
+const winnpattern=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+
+];
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        if (TurnO){
+            box.innerText="O";
+            TurnO=false;
+
+        }else{
+            box.innerText="X";
+            TurnO=true;
+        }
+
+        box.disabled=true;
+        checkwinner();
+    });
+});
+const resetgame=()=>{
+    TurnO=true;
+    enabledboxes();
+    masscontainer.classList.add("hide");
+}
+const disabledboxes=()=>{
+    for(box of boxes){
+        box.disabled=true;
+    }
+}
+const enabledboxes=()=>{
+    for(box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+}
+const showWinner=(winner)=>{
+    massage.innerHTML=`Congrutulation,Winner is ${winner}`;
+    masscontainer.classList.remove("hide");
+    disabledboxes();
+}
+const checkwinner=()=>{
+    for(let pattern of winnpattern){
+        let pos1val=boxes[pattern[0]].innerText;
+        let pos2val=boxes[pattern[1]].innerText;
+        let pos3val=boxes[pattern[2]].innerText;
+        if(pos1val!="" && pos2val!="" && pos3val!=""){
+            if(pos1val===pos2val && pos2val===pos3val){
+                
+                showWinner(pos1val);
+            }
+        }
+    }
+
+};
+newgamebtn.addEventListener("click",resetgame);
+resetbtn.addEventListener("click",resetgame)
